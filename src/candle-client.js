9fe250c14647601339ece9cbab8e046d7ada326d
Sidnei Lacerda <neylacerdayendis@gmail.com>
2026-08-31 23:58:47 -0300

    Corrige suporte a HTTPS no candle-client

--- src/candle-client.js
+++ src/candle-client.js
@@ -1,11 +1,16 @@
 const http = require("http");
+const https = require("https");
+const { URL } = require("url");
 
 const CANDLE_SERVICE_URL = process.env.CANDLE_SERVICE_URL || "http://localhost:5001";
 
 async function fetchCandles(asset, timeframe = 60, amount = 50) {
   return new Promise((resolve, reject) => {
     const url = `${CANDLE_SERVICE_URL}/candles?asset=${asset}&timeframe=${timeframe}&amount=${amount}`;
-    http.get(url, (res) => {
+    const parsed = new URL(url);
+    const client = parsed.protocol === "https:" ? https : http;
+
+    client.get(url, (res) => {
       let data = "";
       res.on("data", chunk => data += chunk);
       res.on("end", () => {
